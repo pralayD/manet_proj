ae496517984e4961e03024455914a4cf20465c0e
pralay <pralay.nitdgp15@gmail.com>
2016-12-08 02:24:32 +0530

    APs created

--- k-means.py
+++ k-means.py
@@ -9,17 +9,17 @@ import random
 m = 0
 
 clusters_AP = []
-clusters = []
+clusters = {}
 
-def create_AP(n_AP,m):
-
+def create_AP(n_AP):
+	global m
 	AP_list = {}
 	for j in range(n_AP):
 		AP_list['AP'+str(m)] = []
 		m += 1
 	return AP_list
 
-def assign_coordinates(m,AP_list):
+def assign_coordinates(c,AP_list):
 	c_h = random.choice(AP_list.keys())
 	
 	(AP_list[c_h]).append(int(raw_input('X-coordinate::')))
@@ -58,10 +58,9 @@ def assign_coordinates(m,AP_list):
 				(AP_list[i]).append(y)	
 
 	for i in AP_list:
-		clusters_AP.append([AP_list[i][0],AP_list[i][1]])
+		clusters[i] = AP_list[i]
 
-	clusters.append(AP_list)
-	print 'c'+str(m)+'::\n'
+	print '::Set -- '+str(c)+'::\n'
 	print AP_list,'\n'
 
 
@@ -69,10 +68,24 @@ def k_means_algo(k):
 	X_Y = np.array(clusters_AP)
 	kmeans = KMeans(n_clusters = k, random_state = 0).fit(X_Y)
 	clust_no = kmeans.labels_
+	print '\n','Cluster Numbers::',clust_no
 
-	#for
-	print clust_no
+	for i,j in zip(clusters,clust_no):
+		clusters[i].append(j)
+
+	print clusters
 	#print kmeans.cluster_centers_
+
+
+def find_clust(n_nodes):
+	global m
+	clust = {}
+	for i in range(n_nodes):
+		clust['AP'+str(m)] = map(int,raw_input().split())
+		m += 1
+	
+	print clust 	
+		
 
 def display(temp_list):		# Display the items in the passed Dictionaries.
 	for keys,values in temp_list.items():
@@ -87,18 +100,27 @@ k = int(raw_input('Enter the number of clusters::\n'))
 
 for i in range(k):
 	n_AP = random.randint(2,5)
-	AP_list = create_AP(n_AP,m)
-	m = n_AP
+	AP_list = create_AP(n_AP)
 
 	assign_coordinates(i,AP_list)
 
-#print clusters
-print clusters_AP
 
-print '\n',clusters,'\n'
+for i in clusters:
+	clusters_AP.append([clusters[i][0],clusters[i][1]])
+
+print '::AP List::'
+print clusters,'\n'
+print 'Co-ordinate List\n',clusters_AP
+
 k_means_algo(k)
-	
-#display(clusters)
+
+display(clusters)
+
+n_nodes = int(raw_input('Enter the number of APs to be predicted::\n'))
+
+find_clust(n_nodes)
+
+
 '''
 X = np.array([[1, 2], [1, 4], [1, 0],[4, 2], [4, 4], [4, 0]])
 kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
